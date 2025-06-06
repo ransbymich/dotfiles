85f778e311ca71f252cffcfc7bb06f34c03621c2
Michael Ransby <38529949+ransbymich@users.noreply.github.com>
2025-06-06 19:58:03 -0400

    chore: remove another mention of fasd

--- zsh/install.sh
+++ zsh/install.sh
@@ -7,14 +7,6 @@ if ! grep -q "$zsh_bin" /etc/shells ; then
   sudo chsh -s "$zsh_bin" "$(whoami)"
 fi
 
-# manually install fasd, as it's archived
-git clone https://github.com/clvv/fasd.git
-cd fasd || exit 1
-make
-PREFIX=$HOME make install
-cd .. || exit 1
-rm -rf fasd
-
 mkdir -p ~/.config
 
 if [ ! -e ~/.config/starship.toml ]; then
